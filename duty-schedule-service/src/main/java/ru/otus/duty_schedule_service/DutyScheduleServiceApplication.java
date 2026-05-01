d6c7a16023f7d66ca2430f28a694f11652f89381
Alexander Stepanov <stepa113@mail.ru>
2026-05-01 14:22:54 +0300

    add duty-schedule-service module

---
+++ duty-schedule-service/src/main/java/ru/otus/duty_schedule_service/DutyScheduleServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.duty_schedule_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyScheduleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyScheduleServiceApplication.class, args);
+	}
+
+}
---
+++ duty-schedule-service/src/test/java/ru/otus/duty_schedule_service/DutyScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.duty_schedule_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyScheduleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
